ac3254acb6da0f0003d40880247b051acd3eba3e
MohamedAbdulmaksoud <mohamed_yasser.22@hotmail.com>
2017-06-17 10:06:41 +0200

    Unity Inspector Scripts
    Display current state of object as well as enabling ID input

---
+++ SensorInspector.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Sensor))]
+public class SensorInspector : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        Sensor sensorScript = (Sensor)target;
+        sensorScript.ID = EditorGUILayout.IntField("ID", sensorScript.ID);
+        EditorGUILayout.LabelField("State", sensorScript.getState().ToString());
+    }
+
+}
+
